65c067ffa593cb3c6a5d3745a0c24b4fcf5aef55
tatjama <tanja120a@gmail.com>
2020-11-12 16:41:27 +0100

    import fontawesome

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import Player from './components/Player';
+import Song from './components/Song';
 
 function App() {
   return (
     <div className="App">
-      <h1>Music player</h1>
+      <Song/>
+      <Player/>
     </div>
   );
 }
---
+++ src/components/Song.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Song = () => {
+    return(
+        <div className = "song-container">
+            <h1>Picture</h1>
+            <h2>Song Name</h2>
+            <h2>Author</h2>
+        </div>
+    )   
+}
+
+export default Song;
+
